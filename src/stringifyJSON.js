06d1b0a906b37463f0bf8d7b0624b19b70f768f8
Kyruski <jamescboyett@gmail.com>
2019-01-02 11:50:48 -0600

    Pseudocode

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //logic for primitive values (bool, string, number)
+  //if (typeof obj 'string' || typeof obj 'boolean')
+    //want to return value as string
+  //if (typeof obj === Infinity || typeof obj === 'boolean')
+  //logic for array
+    //temp array
+    //iterate through
+      // array.push recursion () ['fdsafads', dfsafads ,fdsaffadsf ]
+    //return '[' + recursion + ']'
+
+
+
+  //logic for object
+    //temp aray
+    //iterate through
+      //run recursion
+      //aray.push('' + key + ':' + value)
+    //
+    //return '{' + key + ':' + value + '}'
+
+  //return
+
+
+
 };
