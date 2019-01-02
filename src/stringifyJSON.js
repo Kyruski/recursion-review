0921fdf440315db183eb2a466f26e1630ca94a65
Kyruski <jamescboyett@gmail.com>
2019-01-02 15:56:33 -0600

    Implemented functionality of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,27 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === undefined || typeof obj === 'function' || typeof obj === 'symbol') {
+    return 'undefined';
+  } else if (obj === Infinity || obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return '' + obj + '';
+  } else if (Array.isArray(obj)) {
+    if (obj.length === 0) { return '[]'; }
+    let tempArray = [];
+    for (let i = 0; i < obj.length; i++) {
+      tempArray.push(stringifyJSON(obj[i]));
+    }
+    return '[' + tempArray.join(',') + ']';
+  } else if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) { return '{}'; }
+    let tempObjectArray = [];
+    for (let key in obj) {
+      if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') {
+        continue;
+      }
+      tempObjectArray.push('"' + key + '":' + stringifyJSON(obj[key]));
+    }
+    return '{' + tempObjectArray.join(',') + '}';
+  }
 
-  //logic for primitive values (bool, string, number)
-  //if (typeof obj 'string' || typeof obj 'boolean')
-    //want to return value as string
-  //if (typeof obj === Infinity || typeof obj === 'boolean')
   //logic for array
-    //temp array
-    //iterate through
-      // array.push recursion () ['fdsafads', dfsafads ,fdsaffadsf ]
-    //return '[' + recursion + ']'
+  //temp array
+  //iterate through
+  // array.push recursion () ['fdsafads', dfsafads ,fdsaffadsf ]
+  //return '[' + recursion + ']'
 
 
 
   //logic for object
-    //temp aray
-    //iterate through
-      //run recursion
-      //aray.push('' + key + ':' + value)
-    //
-    //return '{' + key + ':' + value + '}'
+  //temp aray
+  //iterate through
+  //run recursion
+  //aray.push('' + key + ':' + value)
+  //
+  //return '{' + key + ':' + value + '}'
 
   //return
 
