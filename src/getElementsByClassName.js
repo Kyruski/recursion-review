71a9c4b7455cbdd9a08c68cd78acb55f68762858
Kyruski <jamescboyett@gmail.com>
2019-01-02 19:45:04 -0600

    Added fucntionality for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var tempArray = [];
+  var helper = function (location) {
+    if (location.classList) {
+      if (location.classList.contains(className)) {
+        tempArray.push(location);
+      }
+    }
+    console.log(location.childNodes);
+    if (location.childNodes) {
+      for (var i = 0; i < location.childNodes.length; i++) {
+        helper(location.childNodes[i]);
+      }
+    }
+  };
+  helper($(document.body)[0]);
+  return tempArray;
 };
